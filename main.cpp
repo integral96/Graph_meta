f7c6898fccb1bc511ea7a2eef55779be9847042d
Waldemar <41773009+integral96@users.noreply.github.com>
2021-01-02 10:57:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,48 +2,46 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <array>
 
-#include "include/matrix_rings.hpp"
 #include "include/graph.hpp"
+#include "include/matrix_rings.hpp"
+
+static constexpr size_t N = 4;
+
+using List = mpl::vector16<EDGE<'A', 'A', false>, EDGE<'A', 'B', true>, EDGE<'A', 'C', false>, EDGE<'A', 'D', true>,
+                           EDGE<'B', 'B', false>, EDGE<'B', 'C', true>, EDGE<'B', 'D', false>, EDGE<'B', 'A', true>,
+                           EDGE<'C', 'C', false>, EDGE<'C', 'D', true>, EDGE<'C', 'A', false>, EDGE<'C', 'B', true>,
+                           EDGE<'D', 'D', false>, EDGE<'D', 'A', true>, EDGE<'D', 'B', false>, EDGE<'D', 'C', true>>;
 
 
-int main(int argc, char *argv[])
+
+int main()
 {
-    //=====================================
-    matrix_2<5, 5, double> AA{{1, 2, 3, 4,-2},
-                                {-5, 5, 7, 8, 4},
-                                {9, 10, -11, 12, 1},
-                                {13, -14, -15, 0, 9},
-                                {20, -26, 16, -17, 25}};
-    matrix_2<5, 5, double> BB1{{1, 2, 3, 4,-2},
-                                {-5, 5, 7, 8, 4},
-                                {9, 10, -11, 12, 1},
-                                {13, -14, -16, 0, 9},
-                                {20, -26, 16, -17, 25}};
-    std::array<double, 5> vector {1.5, 1.3, 2.5, 1.3, 3.2};
-    std::cout << "AA = \n" << AA << std::endl;
-    std::cout << "BB1 = \n" << BB1 << std::endl;
-    std::cout << "AA * (AA + BB1) = \n" << AA * (AA + BB1) << std::endl;
-    std::cout << "AA * (AA - BB1) = \n" << AA * (AA - BB1) << std::endl;
-    std::cout << "AA^(-1) = \n" << (AA^AA) << std::endl;
-    std::cout << "norma(AA) = " << calc_norma(AA) << std::endl;
-    std::cout << "AA*vector = {";
-    for(const auto& x : AA*vector) std::cout << x << "; ";
-    std::cout << "}" << std::endl;
-    std::cout << "AA/2*AA * (AA*5 + BB1/calc_norma(AA)) = \n" << AA/2*AA * (AA*5 + BB1/calc_norma(AA)) << std::endl;
-
-    std::cout << "identity_matrix(AA) = \n" << AA.identity_matrix() << std::endl;
-    //========================================================
-    auto a = EDGE<'C', 'D', true>::value;
-    auto b = hana::first(a);
-    std::cout << hana::second(b) << std::endl;
-    auto set = GRAPH<EDGE<'C', 'D', true>>::GRAPH_SET;
     std::stringstream out;
-    hana::for_each(set, [&](auto arg) {
-        out << hana::first(hana::first(arg)) <<" ";
+    hana::for_each(GRAPH<N*N, List>::type::value, [&](auto arg) {
+        if (hana::second(arg))
+            out << hana::first(hana::first(arg)) <<" <-> " << hana::second(hana::first(arg)) <<"\n";
+        else
+            out << hana::first(hana::first(arg)) <<" <-/-> " << hana::second(hana::first(arg)) <<"\n";
     });
     std::cout << out.str() << std::endl;
 
-    return 0;
+    bool** matrix = new bool*[N];
+    for(size_t i = 0; i < N; ++i)
+        matrix[i] = new bool[N];
 
+    calc_matrix<N, List>(matrix);
+
+    for (size_t i = 0; i < N; ++i) {
+        for (size_t j = 0; j < N; ++j)
+            std::cout << matrix[i][j] << " ";
+        std::cout << std::endl;
+    }
+
+    for (size_t i = 0; i < N; ++i)
+        delete [] matrix[i];
+    delete [] matrix;
+
+    return 0;
 }
