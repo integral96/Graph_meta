4ce5c55b75ccf61a384c5fe360da90e358ac378e
Waldemar <41773009+integral96@users.noreply.github.com>
2021-01-31 11:21:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,45 +32,57 @@ using SortList2 = InsertionSort<GRAPH<N, List2>::type, SmallThenT>;
 int main()
 {
 
-    std::vector<bool> vec;
+//    std::vector<bool> vec;
+//
+////    std::stringstream out;
+//    hana::for_each(SortList0::value, [&](auto arg) {
+//        if (hana::at_c<1>(hana::first(arg))) {
+//            std::cout << hana::at_c<0>(hana::first(arg)) <<" <-> " << hana::at_c<2>(hana::first(arg)) <<"\n";
+//        } else {
+//            std::cout << hana::at_c<0>(hana::first(arg)) <<" <-/-> " << hana::at_c<2>(hana::first(arg)) <<"\n";
+//        }
+//        if (hana::at_c<1>(hana::second(arg))) {
+//            std::cout << hana::at_c<0>(hana::second(arg)) <<" <-> " << hana::at_c<2>(hana::second(arg)) <<"\n";
+//        } else {
+//            std::cout << hana::at_c<0>(hana::second(arg)) <<" <-/-> " << hana::at_c<2>(hana::second(arg)) <<"\n";
+//        }
+//        vec.push_back(hana::at_c<1>(hana::first(arg)));
+//        vec.push_back(hana::at_c<1>(hana::second(arg)));
+//    });
+//    std::cout << out.str() << std::endl;
+//    vec.erase(vec.begin());
+//    vec.erase(vec.begin());
+//    matrix_2<4, 4> mtx;
+//    for(const auto& x : vec) {
+//        std::cout << "(" << x << "; " << ")\n";
+//    }
+//    std::copy(vec.begin(), vec.end(), mtx.end()->begin());
 
-    std::stringstream out;
-    hana::for_each(SortList0::value, [&](auto arg) {
-        if (hana::at_c<1>(hana::first(arg))) {
-            out << hana::at_c<0>(hana::first(arg)) <<" <-> " << hana::at_c<2>(hana::first(arg)) <<"\n";
-        } else {
-            out << hana::at_c<0>(hana::first(arg)) <<" <-/-> " << hana::at_c<2>(hana::first(arg)) <<"\n";
-        }
-        if (hana::at_c<1>(hana::second(arg))) {
-            out << hana::at_c<0>(hana::second(arg)) <<" <-> " << hana::at_c<2>(hana::second(arg)) <<"\n";
-        } else {
-            out << hana::at_c<0>(hana::second(arg)) <<" <-/-> " << hana::at_c<2>(hana::second(arg)) <<"\n";
-        }
-        vec.push_back(hana::at_c<1>(hana::first(arg)));
-        vec.push_back(hana::at_c<1>(hana::second(arg)));
-    });
-    std::cout << out.str() << std::endl;
-    vec.erase(vec.begin());
-    vec.erase(vec.begin());
-    matrix_2<4, 4> mtx;
-    for(const auto& x : vec) {
-        std::cout << "(" << x << "; " << ")\n";
-    }
-    std::copy(vec.begin(), vec.end(), mtx.end()->begin());
 
 
+//    matrix_2<4, 3, int> mtrx{{1,2,3}, {1,2,3}, {4,2,2}, {4,2,2}};
+//    matrix_2<3, 4, int> mtrx1{{1,2,3,4}, {1,2,3,6}, {1,4,2,2}};
+//    matrix_2<4, 4, int> AA{{1,2,3,4}, {1,2,3,6}, {1,4,2,2}, {1,4,2,2}};
 
-    matrix_2<4, 3> mtrx{{1,2,3}, {1,2,3}, {4,2,2}, {4,2,2}};
-    matrix_2<3, 4> mtrx1{{1,2,3,4}, {1,2,3,6}, {1,4,2,2}};
+//    matrix_2<5, 5, double> AA{{1, 2, 3, 4,-2},
+//                                {-5, 5, 7, 8, 4},
+//                                {9, 10, -11, 12, 1},
+//                                {13, -14, -15, 0, 9},
+//                                {20, -26, 16, -17, 25}};
 ////    mtrx.fill(2);
-    std::cout << mtx << std::endl;
-    std::cout << mtrx1 << std::endl;
-    std::cout << mtrx*mtrx1 << std::endl;
+//    std::cout << mtx << std::endl;
+//    std::cout << mtrx1 << std::endl;
+//    std::cout << mtrx*mtrx1 << std::endl;
+//    auto res = mtrx*mtrx1;
+    matrix_2<12, 12, double> BB;
+    gen_rand_matrix(BB, 1.1, 50.5);
+    std::cout << BB << std::endl;
+    std::cout << (BB^BB) << std::endl;
 
 //    matrix_3<4, 3, 2, int> matrx3d;
 //    gen_rand_matrix(matrx3d, 1,5);
 //    std::cout << matrx3d << std::endl;
-
+//
 //    matrix_4<4, 3, 2, 5, int> matrx4d;
 //    gen_rand_matrix(matrx4d, 1,5);
 //    std::cout << matrx4d << std::endl;
